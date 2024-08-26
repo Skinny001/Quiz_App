4c7e0c6c8d82aec9aeb66f6655624fa21142a29e
Skinny001 <bamigboyetemitope84@gmail.com>
2024-08-26 12:32:20 +0100

    not yet done

---
+++ script.js
@@ -0,0 +1,5 @@
+//getting all required elements
+const start_btn = document.querySelector(".start_btn button");
+const info_box = document.querySelector(".info_box");
+const quit_btn = document.querySelector(".info_box");
+const info_box = document.querySelector(".info_box");
